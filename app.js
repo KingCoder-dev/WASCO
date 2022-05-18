00275fe55018c93d0bc72e541e6a9511ef80e2cb
KingCoder-dev <53541181+KingCoder-dev@users.noreply.github.com>
2022-05-17 20:55:52 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const submit = document.getElementById("name");
+const answer = document.getElementById("submit");
+const form = document.getElementById("form");
+const select = document.getElementById("subject");
+const select2 = document.getElementById("subject2");
+const select3 = document.getElementById("subject3");
+const position1 = document.getElementById("position1");
+const decription1 = document.getElementById("description1");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (
+    (select.value == "tech-drawing" ||
+      select2.value == "info-tech" ||
+      select3.value == "geo")
+    &&
+    (select.value == "info-tech" ||
+      select2.value == "tech-drawing" ||
+      select3.value == "geo")
+    &&
+    (select.value == "geo" ||
+      select2.value == "tech-drawing" ||
+      select3.value == "info-tech")
+  ) {
+    window.location = "positions1.html";
+
+  }
+  if ((select.value == 'bio' || select.value == 'chem' || select.value == "physics") &&
+    (select2.value == 'bio' || select2.value == 'chem' || select2.value == "physics") &&
+    (select3.value == 'bio' || select3.value == 'chem' || select3.value == "physics")) {
+
+    window.location = 'positions2.html'
+  }
+  if ((select.value == 'plumber' || select.value == 'eng')
+    && (select2.value == 'plumber' || select2.value == 'eng') &&
+    (select3.value == 'plumber' || select3.value == 'eng')) {
+    window.location = "position4.html"
+  }
+  if ((select.value == "qs") &&
+    (select2.value == "qs") &&
+    (select3.value == "qs")) {
+    window.location = 'position3.html'
+  }
+  else
+    window.location = 'positions.html'
+});
